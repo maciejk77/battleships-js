f383e8b7e87f24ed23056e466424a6fcba594f34
Maciej Kurek <maciejk77@gmail.com>
2016-07-29 18:31:58 +0100

    README update no.1

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-Battleships game in JS
+#Battleships in Javascript
+
+The purpose of this exercise is to create a game engine in Javascript and UI.
+
+##Instruction
+
+- addShip(length, orientation, xCoordinate, yCoordinate)
+- hit by clicking on individual cells (event handling of click and changing CSS styles)
+
+###To be done
+
+- error handling for off the grid ship placement
+- random ship placements and hits (AI player vs computer)
+- storing number of hits per ship and alert when sunken
+- method to return a status of each ship, number of hits etc.
+
+####Preview
+
+![battleships-js screenshot](https://github.com/maciejk77/)
--- app.js
+++ app.js
@@ -4,96 +4,134 @@ var yLength = 10;
 var squareSize = 50;
 var shipsSquares = 17;
 
-// get container element by id
-var gameBoardContainer = document.getElementById("gameboard");
-
-// create grid by manipulating DOM and CSS
-for (i = 0; i < yLength; i++) {
-  for (j = 0; j < xLength; j++) {
-
-    // an array of letters to add to Y coordinates and to each square of the grid
-    var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
-
-    // create div for each box and set the size
-    var square = document.createElement("div");
-    gameBoardContainer.appendChild(square);
-
-    // give each div element a unique id based on its xCoord and yCoordumn
-    square.id = 's' + j + i;
-    square.innerText = letters[j] + '' + (i + 1);      
-    
-    // set each grid square's coordinates: multiples of the current xCoord or yCoordumn number
-    var topXY = j * squareSize;
-    var leftXY = i * squareSize;      
-    
-    // use CSS absolute positioning to place each grid square on the page
-    square.style.top = topXY + 'px';
-    square.style.left = leftXY + 'px';            
-  }
-}
-
-/* 
-      Carrier     - 5 hits
-      Battleship  - 4 hits
-      Destroyer   - 3 hits
-      Submarine   - 3 hits
-      Patrol Boat - 2 hits
-      Total - 17 hits required to sunk all
-*/
+// Carrier 5 hits | Battleship 4 hits | Destroyer 3 hits | Submarine 3 hits | Patrol Boat 2 hits
+// Total - 17 hits required to sunk all
 
 // counter to increment up to 17, sum of ship squares, all sunk
 var hitsCounter = 0;
 
-// 0 = empty, 1 = part of a ship, 2 = a sunken part of a ship, 3 = a missed shot
+// get container element by id
+var GameBoardContainer = document.getElementById("gameboard");
 
-var gameBoard = [
-        [0,0,0,1,1,1,1,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,0,0,0,0],
-        [0,0,0,0,0,0,1,0,0,0],
-        [0,0,0,0,0,0,1,0,0,0],
-        [1,0,0,0,0,0,1,1,1,1],
-        [1,0,0,0,0,0,0,0,0,0],
-        [1,0,0,1,0,0,0,0,0,0],
-        [1,0,0,1,0,0,0,0,0,0],
-        [1,0,0,0,0,0,0,0,0,0]
-        ]
+// specifying size of board and access to the grid array
+var boardObj = new GameBoard(10, 10); 
+var board = boardObj.tiles;
 
-// set event listener for all elements in gameboard, run fire function when square is clicked
-gameBoardContainer.addEventListener("click", fire, false);
+// generating game board object
+function GameBoard (xLength, yLength) {
+  this.tiles = []; // will represent the games board
+  this.ships = []; // will contain information on each ship added to the board
+
+  for (var x = 0; x < xLength; x++) {
+    for (var y = 0; y < yLength; y++) {
+      if (!this.tiles[x]) this.tiles[x] = [];
+      this.tiles[x][y] = 0;
+    }
+  }
+}
+
+// create grid by manipulating DOM and CSS
+function getUIGrid() { 
+
+  for (i = 0; i < yLength; i++) {
+    for (j = 0; j < xLength; j++) {
+
+      // an array of letters to add to Y coordinates and to each square of the grid
+      var letters = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+
+      // create div for each box and set the size
+      var square = document.createElement("div");
+      GameBoardContainer.appendChild(square);
+
+      // give each div element a unique id based on its xCoord and yCoordumn
+      square.id = 's' + j + i;
+      square.innerText = letters[j] + '' + (i + 1);      
+      
+      // set each grid square's coordinates: multiples of the current xCoord or yCoordumn number
+      var topXY = j * squareSize;
+      var leftXY = i * squareSize;      
+      
+      // use CSS absolute positioning to place each grid square on the page
+      square.style.top = topXY + 'px';
+      square.style.left = leftXY + 'px';            
+    }
+  }
+
+}  
+
+function addShip(length, orientation, xCoord, yCoord) {
+
+  for(i = 0; i < length; i++) {
+    var tempArr = [];
+    
+    if(board[xCoord][yCoord] === 0) {
+      board[xCoord][yCoord] = 1;
+      
+      switch(orientation) {
+        case 'right':
+          console.log('right');
+          xCoord++;
+          break;
+        case 'left':
+          console.log('left');
+          xCoord--;
+          break;
+        case 'up':
+          console.log('up');
+          yCoord--;
+          break;
+        case 'down':
+          console.log('down');
+          yCoord++;
+          break;  
+      } 
+    
+    } 
+  
+  }
+
+  return true;
+}
 
 function fire(e) {
     // if item clicked (e.target) is not the parent element on which the event listener was set (e.currentTarget)
   if (e.target !== e.currentTarget) {
     // extract xCoord and yCoord from the HTML element's id
-    var xCoord = e.target.id.substring(1,2);
-    var yCoord = e.target.id.substring(2,3);
-        
+    var xCoord = e.target.id.substring(2,3);
+    var yCoord = e.target.id.substring(1,2);   
+    // 0 -> empty, 1 -> part of a ship, 2 -> a sunken part of a ship, 3 -> a missed shot
+    
     // if player clicks a square with no ship, change the yCoordor and change square's value
-    if (gameBoard[xCoord][yCoord] === 0) {
+    if (board[xCoord][yCoord] === 0) {
       e.target.style.background = '#bbb';
       // set this square's value to 3 to indicate that they fired and missed
       console.log('fire ' + xCoord + yCoord + ' ===> miss');
-      gameBoard[xCoord][yCoord] = 3;
+      board[xCoord][yCoord] = 3;
       
     // if player clicks a square with a ship, change the yCoordor and change square's value
-    } else if (gameBoard[xCoord][yCoord] === 1) {
+    } else if (board[xCoord][yCoord] === 1) {
       e.target.style.background = 'red';
       // set this square's value to 2 to indicate the ship has been hit
       console.log('fire ' + xCoord + yCoord + ' ===> hit');
-      gameBoard[xCoord][yCoord] = 2;
+      board[xCoord][yCoord] = 2;
       
       // increment hitsCounter each time a ship is hit
       hitsCounter++;
       // hitsCounter reached the limit, all sunken ships.
       if (hitsCounter === shipsSquares) {
-        console.log('All ships destroyed! GAME');
+        console.log('All ships destroyed, GAME OVER!');
       }
       
     // if player clicks a square that's been previously hit, a warning appears
-    } else if (gameBoard[xCoord][yCoord] > 1) {
+    } else if (board[xCoord][yCoord] > 1) {
       console.log('fire ' + xCoord + yCoord + ' ===> already hit');
     }   
   }
+  e.target.innerText = ''; 
   e.stopPropagation();
 }
+
+getUIGrid();
+
+// set event listener for all elements in gameboard, run fire function when square is clicked
+GameBoardContainer.addEventListener("click", fire, false);
